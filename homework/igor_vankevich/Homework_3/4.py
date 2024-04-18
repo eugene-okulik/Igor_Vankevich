fdd26e1fded69e31ab4a7b0f9a774954975354b8
Igor_Vankevich <karterslayz@gmail.com>
2024-04-18 14:25:38 +0300

    fix missing placeholders in 4.py

--- homework/igor_vankevich/Homework_3/4.py
+++ homework/igor_vankevich/Homework_3/4.py
@@ -1,4 +1,4 @@
 a = 10
 b = 6
-print(f"hypotenuse =", ((a ** 2) + (b ** 2)) ** 0.5)
-print(f"areas of triangles =", (a * b) / 2)
+print("hypotenuse =", ((a ** 2) + (b ** 2)) ** 0.5)
+print("areas of triangles =", (a * b) / 2)

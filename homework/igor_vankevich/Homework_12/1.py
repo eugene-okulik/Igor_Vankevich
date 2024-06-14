4a93010e3efd31e5f092b8142445e734f0679b97
Igor_Vankevich <karterslayz@gmail.com>
2024-06-14 11:54:03 +0300

    update on comments

--- homework/igor_vankevich/Homework_12/1.py
+++ homework/igor_vankevich/Homework_12/1.py
@@ -1,7 +1,8 @@
 class Flowers:
     kind = 'plants'
 
-    def __init__(self, cost, freshness, size, life_time):
+    def __init__(self, name, cost, freshness, size, life_time):
+        self.name = name
         self.cost = cost
         self.freshness = freshness
         self.size = size
@@ -12,82 +13,99 @@ class GardenFlowers(Flowers):
     type = 'garden'
     color = 'red'
 
-    def __init__(self, cost, freshness, size, life_time, name):
-        super().__init__(cost, freshness, size, life_time)
-        self.name = name
+    def __init__(self, name, cost, freshness, size, life_time):
+        super().__init__(name, cost, freshness, size, life_time)
 
 
 class WildFlowers(Flowers):
     type = 'wild'
     color = 'green'
 
-    def __init__(self, cost, freshness, size, life_time, name):
-        super().__init__(cost, freshness, size, life_time)
-        self.name = name
+    def __init__(self, name,  cost, freshness, size, life_time):
+        super().__init__(name, cost, freshness, size, life_time)
 
 
 class ForestFlowers(Flowers):
     type = 'forest'
     color = 'blue'
 
-    def __init__(self, cost, freshness, size, life_time, name):
-        super().__init__(cost, freshness, size, life_time)
-        self.name = name
+    def __init__(self, name,  cost, freshness, size, life_time):
+        super().__init__(name, cost, freshness, size, life_time)
 
 
 class BouquetFlowers:
 
     list_flowers = []
 
-    def __init__(self, flowers1, flowers2, flowers3, flowers4):
-        self.flowers1 = flowers1
-        self.flowers2 = flowers2
-        self.flowers3 = flowers3
-        self.flowers4 = flowers4
-        list_flowers = [flowers1, flowers2, flowers3, flowers4]
-        for x in list_flowers:
+    def __init__(self, *flower):
+        for x in flower:
             BouquetFlowers.list_flowers.append(x)
-        print(f'Cost bouquet = {flowers1.cost + flowers2.cost + flowers3.cost + flowers4.cost}')
 
-    def wilting_time(self):
-        wilting = (self.flowers1.life_time + self.flowers2.life_time + self.flowers3.life_time
-                   + self.flowers4.life_time) / 4
-        return print(f'Wilting = {wilting}')
+    @staticmethod
+    def wilting_time():
+        i = 0
+        a = []
+        while i < len(BouquetFlowers.list_flowers):
+            a.append(BouquetFlowers.list_flowers[i].life_time)
+            i += 1
+        print(sum(a) / len(BouquetFlowers.list_flowers))
 
-    def sort_size(self):
-        print(', '.join(map(str, sorted([self.flowers1.size, self.flowers2.size, self.flowers3.size,
-                                         self.flowers4.size]))))
+    @staticmethod
+    def sort_size():
+        i = 0
+        a = []
+        while i < len(BouquetFlowers.list_flowers):
+            a.append(BouquetFlowers.list_flowers[i].size)
+            i += 1
+        print(sorted(a))
 
-    def sort_freshness(self):
-        print(', '.join(map(str, sorted([self.flowers1.freshness, self.flowers2.freshness, self.flowers3.freshness,
-                                         self.flowers4.freshness]))))
+    @staticmethod
+    def sort_freshness():
+        i = 0
+        a = []
+        while i < len(BouquetFlowers.list_flowers):
+            a.append(BouquetFlowers.list_flowers[i].freshness)
+            i += 1
+        print(sorted(a))
 
-    def sort_cost(self):
-        print(', '.join(map(str, sorted([self.flowers1.cost, self.flowers2.cost, self.flowers3.cost,
-                                         self.flowers4.cost]))))
+    @staticmethod
+    def sort_cost():
+        i = 0
+        a = []
+        while i < len(BouquetFlowers.list_flowers):
+            a.append(BouquetFlowers.list_flowers[i].cost)
+            i += 1
+        print(sorted(a))
 
-    def sort_color(self):
-        print(', '.join(sorted([self.flowers1.color, self.flowers2.color, self.flowers3.color, self.flowers4.color])))
+    @staticmethod
+    def sort_color():
+        i = 0
+        a = []
+        while i < len(BouquetFlowers.list_flowers):
+            a.append(BouquetFlowers.list_flowers[i].color)
+            i += 1
+        print(sorted(a))
 
-    def search_name(self):
+    @staticmethod
+    def search_name():
         user_input = input('enter a name\n')
-        if user_input == self.flowers1.name or self.flowers2.name or self.flowers2.name or self.flowers3.name\
-                or self.flowers4.name:
-            print(f'{user_input} in bouquet')
-        else:
-            print('the parameter will not find')
+        i = 0
+        while i < len(BouquetFlowers.list_flowers):
+            if user_input == BouquetFlowers.list_flowers[i].name:
+                print(f'{user_input} in bouquet')
+            i += 1
 
 
-flower1 = GardenFlowers(12, 10, 5, 10, 'rose')
-flower2 = GardenFlowers(4, 6, 7, 10, 'dahlias')
-flower3 = WildFlowers(22, 11, 10, 15, 'chamomile')
-flower4 = WildFlowers(42, 9, 12, 15, 'clover')
-flower5 = ForestFlowers(2, 15, 15, 17, 'lily')
-flower6 = ForestFlowers(8, 17, 21, 17, 'violet')
+flower1 = GardenFlowers('rose', 12, 10, 5, 10)
+flower2 = GardenFlowers('dahlias', 4, 6, 7, 10)
+flower3 = WildFlowers('chamomile', 22, 11, 10, 15)
+flower4 = WildFlowers('clover', 42, 9, 12, 15)
+flower5 = ForestFlowers('lily', 2, 15, 15, 17)
+flower6 = ForestFlowers('violet', 8, 17, 21, 17)
 bouquet1 = BouquetFlowers(flower1, flower4, flower5, flower6)
-BouquetFlowers.wilting_time(bouquet1)
-BouquetFlowers.sort_size(bouquet1)
-BouquetFlowers.sort_cost(bouquet1)
-BouquetFlowers.sort_freshness(bouquet1)
-BouquetFlowers.sort_color(bouquet1)
-BouquetFlowers.search_name(bouquet1)
+BouquetFlowers.wilting_time()
+BouquetFlowers.sort_size()
+BouquetFlowers.sort_cost()
+BouquetFlowers.sort_freshness()
+BouquetFlowers.sort_color()
+BouquetFlowers.search_name()
